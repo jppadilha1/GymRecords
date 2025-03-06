ba3effa365762635101a571aa65f25ad6f4fd1bc
jppadilha1 <100085279+jppadilha1@users.noreply.github.com>
2025-03-06 22:13:01 +0000

    finish endpoint `/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,29 @@
 import database from "infra/database.js";
+import { version } from "react";
 
 async function status(request, response) {
-  const testequery = await database.query("SELECT 5+123 as SUM;");
+  const timeupdated = new Date().toISOString();
+
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersionResult.rows[0].server_version;
+
+  const maxconnectionsResult = await database.query("SHOW max_connections;");
+  const maxconnectionsValue = maxconnectionsResult.rows[0].max_connections;
+
+  const openedConnectionsResult = await database.query(
+    " SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db'"
+  );
+  const opennedConnectionsValue = openedConnectionsResult.rows[0].count;
 
   response.status(200).json({
-    teste: testequery.rows[0],
+    updated_at: timeupdated,
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: parseInt(maxconnectionsValue),
+        openned_connections: opennedConnectionsValue,
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,15 @@
 test("get to api/v1/status should be return 200", async function () {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  updatedAtParseISO = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toBe(updatedAtParseISO);
+
+  expect(responseBody.dependencies.database.version).toBe("16.0");
+
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+
+  expect(responseBody.dependencies.database.openned_connections).toBe(1);
 });
