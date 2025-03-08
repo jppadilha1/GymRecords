60a9ca87d526d2b389894c1e5a17837359a9de7b
jppadilha1 <100085279+jppadilha1@users.noreply.github.com>
2025-03-08 13:02:45 +0000

    prevent SQL injection in `api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,4 @@
 import database from "infra/database.js";
-import { version } from "react";
 
 async function status(request, response) {
   const timeupdated = new Date().toISOString();
@@ -10,9 +9,12 @@ async function status(request, response) {
   const maxconnectionsResult = await database.query("SHOW max_connections;");
   const maxconnectionsValue = maxconnectionsResult.rows[0].max_connections;
 
-  const openedConnectionsResult = await database.query(
-    " SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db'"
-  );
+  const databaseName = process.env.POSTGRES_DB;
+
+  const openedConnectionsResult = await database.query({
+    text: " SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
   const opennedConnectionsValue = openedConnectionsResult.rows[0].count;
 
   response.status(200).json({
