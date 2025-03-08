389338d1861604871d7333e415c6b63a89422778
jppadilha1 <100085279+jppadilha1@users.noreply.github.com>
2025-03-08 13:29:38 +0000

    add ssl configuration method

--- infra/database.js
+++ infra/database.js
@@ -2,13 +2,13 @@ import { Client } from "pg";
 
 async function query(object) {
   const client = new Client({
-    host: process.env.POSTGRES_HOST,
+    host: process.env.POSTGRES_LOCALHOST,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
-
   try {
     await client.connect();
     const result = await client.query(object);
